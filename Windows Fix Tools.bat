30d78f5e491353b13b6094ce68225966ee3c266d
Gabriel Garcia <142952247+GabrielGarcia10@users.noreply.github.com>
2023-11-22 18:09:50 -0300

    v1.1

--- Windows Fix Tools.bat
+++ Windows Fix Tools.bat
@@ -84,19 +84,31 @@ IF %M%==2 GOTO Portugues
 
 :English
 SET menu1Op1=Automated correction
-SET menu1Op2=Advanced options
+SET menu1Op2=Repair disk
+SET menu1Op3=Windows update fix
+SET menu1Op4=Advanced options
 
-SET menuOp1="SFC & DISM" check and automatic correction
-SET menuOp2="SFC & DISM" check and manual correction
+SET menu2Op1="SFC & DISM" check and automatic correction
+SET menu2Op2="SFC & DISM" check and manual correction
+
+SET menuWinUpdOp1=Solution 1
+SET menuWinUpdOp2=Solution 2 - repair the disk
+SET menuWinUpdOp3=Solution 3 - repair "DISM" and "SFC" system
 
 GOTO start
 
 :Portugues
 SET menu1Op1=Correcao automatizada
-SET menu1Op2=Opcoes avancadas
+SET menu1Op2=Reparar disco
+SET menu1Op3=Correcao do windows update
+SET menu1Op4=Opcoes avancadas
 
 SET menu2Op1=Verificacao "SFC & DISM" e correcao automatica
 SET menu2Op2=Verificacao "SFC & DISM" e correcao manual
+
+SET menuWinUpdOp1=Solucao 1
+SET menuWinUpdOp2=Solucao 2 - repare o disco
+SET menuWinUpdOp3=Solucao 3 - repare o sistema "DISM" e "SFC"
 
 GOTO start
 
@@ -117,13 +129,17 @@ ECHO =============================
 ECHO.
 ECHO 1 - %menu1Op1%
 ECHO 2 - %menu1Op2%
-ECHO 3 - EXIT
+ECHO 3 - %menu1Op3%
+ECHO 4 - %menu1Op4%
+ECHO 5 - EXIT
 ECHO.
-SET /P M=Type 1, 2 or 3 then press ENTER: 
+SET /P M=Type 1, 2, 3, 4 or 5 then press ENTER: 
 IF %M%==1 GOTO auto
-IF %M%==2 GOTO MENU2
-IF %M%==3 GOTO FIM
-IF %N%==4 GOTO EOF
+IF %M%==2 GOTO FixDisk
+IF %M%==3 GOTO MENUWinUpdateFix
+IF %M%==4 GOTO MENU2
+IF %M%==5 GOTO FIM
+IF %N%=>6 GOTO EOF
 
 
 
@@ -141,7 +157,7 @@ ECHO.
 SET /P M=Type 1, 2 or 3 then press ENTER: 
 IF %M%==1 GOTO AUTOCMD
 IF %M%==2 GOTO MANUALCMD
-IF %M%==3 GOTO FIM
+IF %M%==3 GOTO start
 IF %N%==4 GOTO EOF
 
 
@@ -178,6 +194,52 @@ echo on
 powershell -command "sfc /scannow; Dism /Online /Cleanup-Image /ScanHealth; dism /online /cleanup-image /CheckHealth"
 echo off
 GOTO FIM
+
+
+:FixDisk
+ECHO.
+ECHO =============================
+ECHO Fix Disk:
+ECHO.
+chkdsk/f C:
+GOTO FIM
+
+:MENUWinUpdateFix
+CLS
+ECHO.
+ECHO =============================
+ECHO Windows Fix Tools
+ECHO =============================
+ECHO.
+ECHO =============================
+ECHO Windows Update Fix:
+ECHO.
+ECHO 1 - %menuWinUpdOp1%
+ECHO 2 - %menuWinUpdOp2%
+ECHO 3 - %menuWinUpdOp3%
+ECHO 4 - EXIT
+ECHO.
+SET /P M=Type 1, 2, 3 or 4 then press ENTER: 
+IF %M%==1 GOTO WinUpdateFixSolution1
+IF %M%==2 GOTO FixDisk
+IF %M%==3 GOTO AUTOCMD
+IF %M%==4 GOTO start
+IF %N%==5 GOTO EOF
+
+:WinUpdateFixSolution1
+ECHO.
+ECHO =============================
+ECHO Windows Update Fix:
+ECHO.
+net stop bits
+net stop wuauserv
+ren %systemroot%\softwaredistribution softwaredistribution.bak
+ren %systemroot%\system32\catroot2 catroot2.bak
+net start bits
+net start wuauserv 
+SET /P M=Reinicar agora? / Restart now? (Y=1/N): 
+IF %M%==1 shutdown /r /t 0
+GOTO start
 
 :FIM
 ECHO.
